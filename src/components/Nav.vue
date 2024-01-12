365338bdea4c0261a3e3794906ab11daf09dc1df
wanghao <1613104610@qq.com>
2024-01-12 17:42:47 +0800

    Add type module to package.json and update stylelint-config version
    Update Nav.vue component styling

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex-b-c">
     <div v-for="item in navList" :key="item.name" class="navItem" @click="gotoFn(item)">
-      <div v-if="item.icon" >
-        <img class="w-20px h-20px" :src="item.icon" :title="item.name">
+      <div v-if="item.icon">
+        <img class="h-20px w-20px" :src="item.icon" :title="item.name">
       </div>
       <div v-else>{{ item.name }}</div>
     </div>
@@ -10,30 +10,30 @@
 </template>
 
 <script setup lang="ts">
-import { router } from '@/router/route';
+import { router } from '@/router/route'
 
 interface NavItem {
   name: string
   path: string
   icon?: string | undefined
 }
-const props = defineProps<{navList:Array<NavItem>}>()
+const props = defineProps<{ navList: Array<NavItem> }>()
 
-function gotoFn(params:NavItem) {
-  if(params.path.includes('http')){
+function gotoFn(params: NavItem) {
+  if (params.path.includes('http')) {
     window.open(params.path)
     return
   }
   router.push(params.path)
 }
-
 </script>
 
 <style lang="scss" scoped>
-.navItem{
+.navItem {
   margin: 0 10px;
   cursor: pointer;
-  &:hover{
+
+  &:hover {
     color: #409eff;
   }
 }
